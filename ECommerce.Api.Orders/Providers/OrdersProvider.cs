60ebe4004805012b4677cf9d350d84fd169e4a1e
Jay Raja <jayraja15@hotmail.com>
2022-07-19 16:46:39 +0100

    Add project files.

---
+++ ECommerce.Api.Orders/Providers/OrdersProvider.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using ECommerce.Api.Orders.Db;
+using ECommerce.Api.Orders.Interfaces;
+using ECommerce.Api.Orders.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Orders.Providers
+{
+    public class OrdersProvider : IOrdersProvider
+    {
+        private readonly OrdersDbContext dbContext;
+        private readonly ILogger<OrdersProvider> logger;
+        private readonly IMapper mapper;
+
+        public OrdersProvider(OrdersDbContext dbContext, ILogger<OrdersProvider> logger, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.logger = logger;
+            this.mapper = mapper;
+
+            SeedData();
+        }
+
+        private void SeedData()
+        {
+            if (!dbContext.Orders.Any())
+            {
+                dbContext.Orders.Add(new Db.Order() 
+                { 
+                    Id = 1, CustomerId = 1, Items = new List<Db.OrderItem> 
+                    { 
+                        new Db.OrderItem
+                        {
+                            Id = 1,
+                            OrderId = 1,
+                            ProductId = 1,
+                            Quantity = 2,
+                            UnitPrice = 30
+                        }
+                    } 
+                });
+                dbContext.Orders.Add(new Db.Order()
+                {
+                    Id = 2,
+                    CustomerId = 1,
+                    Items = new List<Db.OrderItem>
+                    {
+                        new Db.OrderItem
+                        {
+                            Id = 2,
+                            OrderId = 2,
+                            ProductId = 3,
+                            Quantity = 5,
+                            UnitPrice = 50
+                        }
+                    }
+                });
+                dbContext.SaveChanges();
+            }
+        }
+
+        public async Task<(bool IsSuccess, IEnumerable<Models.Order> Orders, string ErrorMessage)> GetOrdersAsync(int customerId)
+        {
+            try
+            {
+                var orders = await dbContext.Orders.Where(o => o.CustomerId == customerId).ToListAsync();
+                if (orders != null && orders.Any())
+                {
+                    var result = mapper.Map<IEnumerable<Db.Order>, IEnumerable<Models.Order>>(orders);
+                    return (true, result, null);
+                }
+                return (false, null, "Not found");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+    }
+}
